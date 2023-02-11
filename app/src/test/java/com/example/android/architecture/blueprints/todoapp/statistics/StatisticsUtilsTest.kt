a118a8ff929c31c418418f4cc0ee5542e0bf1a15
P-BUS <pavlo.buslowski@gmail.com>
2023-02-11 13:59:32 +0200

    fun changed to pass the tests

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -1,6 +1,8 @@
 package com.example.android.architecture.blueprints.todoapp.statistics
 
 import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -8,14 +10,67 @@ class StatisticsUtilsTest {
 
     @Test
     fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
-        // Create an active task
+        // Arrange. Create an active task
         val tasks = listOf(
             Task("title", "desc", isCompleted = false)
         )
-        // Call your function
+        // Act. Call your function
         val result = getActiveAndCompletedStats(tasks)
-        // Check the result
-        assertEquals(result.completedTasksPercent, 0f)
-        assertEquals(result.activeTasksPercent, 100f)
+        // Assert. Check the result
+        assertThat(result.completedTasksPercent, `is`(0f)) // Modified with Hamcrest
+        assertEquals(result.activeTasksPercent, 100f) // Usual Junit expression
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_noActive_returnsZeroHundred() {
+        // Arrange. Create an active task
+        val tasks = listOf(
+            Task("title", "desc", isCompleted = true)
+        )
+        // Act. Call your function
+        val result = getActiveAndCompletedStats(tasks)
+        // Assert. Check the result
+        assertThat(result.completedTasksPercent, `is`(100f)) // Modified with Hamcrest
+        assertEquals(result.activeTasksPercent, 0f) // Usual Junit expression
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_twoCompletedThreeActive_returnsForteSixte() {
+        // Arrange. Create an active task
+        val tasks = listOf(
+            Task("title", "desc", isCompleted = true),
+            Task("title", "desc", isCompleted = true),
+            Task("title", "desc", isCompleted = true),
+            Task("title", "desc", isCompleted = false),
+            Task("title", "desc", isCompleted = false),
+            Task("title", "desc", isCompleted = false)
+        )
+        // Act. Call your function
+        val result = getActiveAndCompletedStats(tasks)
+        // Assert. Check the result
+        assertThat(result.completedTasksPercent, `is`(40f)) // Modified with Hamcrest
+        assertEquals(result.activeTasksPercent, 60f) // Usual Junit expression
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_emptyList_returnZero() {
+        // Arrange. Create an active task
+        val tasks = listOf<Task>()
+        // Act. Call your function
+        val result = getActiveAndCompletedStats(tasks)
+        // Assert. Check the result
+        assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent, `is`(0f))
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_null_returnZero() {
+        // Arrange. Create an active task
+        val tasks = null
+        // Act. Call your function
+        val result = getActiveAndCompletedStats(tasks)
+        // Assert. Check the result
+        assertThat(result.activeTasksPercent, `is`(0f))
+        assertThat(result.completedTasksPercent, `is`(0f))
     }
 }
